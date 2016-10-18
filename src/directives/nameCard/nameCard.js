83bd6ad4184b70fab774f8a7b9ce6f431b020a38
AtillaSzikora <szikora.atilla@gmail.com>
2016-10-19 00:09:36 +0200

    Root folder renamed from app to src

--- src/directives/nameCard/nameCard.js
+++ src/directives/nameCard/nameCard.js
@@ -10,15 +10,11 @@ app.factory('nameCardCtrl', ['$http', function ($http) {
     return function () {
         var vm = this;
         vm.cardCounter = 3;
-
         vm.httpGet = function() {
             vm.url = "http://www.filltext.com/?rows=" + vm.cardCounter +
-                "&fname={firstName}&" +
-                "lname={lastName}&" +
-                "company={business}&" +
-                "city={city}&" +
-                "email={email}&" +
-                "phone={phone|format}&";
+                     "&fname={firstName}&" + "lname={lastName}&" +
+                     "company={business}&" + "city={city}&" +
+                     "email={email}&" + "phone={phone|format}&";
 
             $http.get(vm.url)
                 .then(function Success(response) {
